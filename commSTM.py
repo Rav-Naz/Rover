8bf028cdb1b6fa2e0f34bd54667cb0853a50f9e4
micr3 <michal.cieszynski5@gmail.com>
2021-01-14 20:42:02 +0100

    to test with STM or arduino

---
+++ commSTM.py
@@ -0,0 +1,38 @@
+#comunication RPI->STM
+#send information via I2C
+#RPI Master STM Slave
+
+from smbus2 import SMBus
+import time
+
+# I2C address of Slaves
+i2c_address = 0x07 #zmienic
+
+bus=smbus.SMBus(1)
+
+data_received = ""
+data_send = "test" #functionwrite_i2c_block_data() send max 32byte
+
+def writeData(value):
+    byteValue = StringToBytes(value)
+    bus.write_i2c_block_data(i2c_address, 0x00, byteValue) #first byte is 0=command byte.. just is xD
+    return -1
+
+def StringToBytes(val):
+        retVal = []
+        for c in val:
+                retVal.append(ord(c))
+        return retVal
+    
+while(True):
+    print('sending')
+    writeData(data_send)
+    time.sleep(5)
+
+    print("sending")
+    writeData("test")
+    time.sleep(5)
+
+    print("sending")
+    writeData("test_dluzszy1234")
+    time.sleep(5)
